a7c8696600b1dbf2cd7aecf6970a5bdc1ef39d10
KowsikaMuralikrishnan <kowsikamuralikrishnan2304@gmail.com>
2025-09-10 12:22:30 +0530

    Update IsEven.java

--- Day01_IsEven/IsEven.java
+++ Day01_IsEven/IsEven.java
@@ -1,10 +1,7 @@
-public class IsEven {
-    public static void main(String[] args) {
-        int n = 10;
-        if (n % 2 == 0) {
-            System.out.println("Even");
-        } else {
-            System.out.println("Odd");
-        }
+public int IsEven(int input1) {
+    if (input1 % 2 == 0) {
+        return 2;  // Even
+    } else {
+        return 1;  // Odd
     }
 }
