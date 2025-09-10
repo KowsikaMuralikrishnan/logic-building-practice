168fbe4a8c57a2afb635d3b6aad13da8e7d3e84d
KowsikaMuralikrishnan <kowsikamuralikrishnan2304@gmail.com>
2025-09-10 12:13:41 +0530

    Create IsEven.java

---
+++ Day01_IsEven/IsEven.java
@@ -0,0 +1,10 @@
+public class IsEven {
+    public static void main(String[] args) {
+        int n = 10;
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
